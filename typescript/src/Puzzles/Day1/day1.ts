760104b7ccd6c2f025814c2a64875eacfd7c2891
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2023-12-01 01:19:44 -0600

    added notes

--- typescript/src/Puzzles/Day1/day1.ts
+++ typescript/src/Puzzles/Day1/day1.ts
@@ -3,14 +3,17 @@ const day1 = (data: string) => {
 
   let sum = 0;
 
-  linesOfText.forEach((line) => {
+  linesOfText.forEach((line, index) => {
     //Maybe I can use the string it self to search through.
     const lineOfArray = line.split("");
 
     // THIS is the right way of thinking.
     const leftFind = findFirst(lineOfArray);
+    // how i made this functions with a flag feels wrong.
     const rightFind = findFirst(lineOfArray.reverse(), "do");
+    // Logs
     console.log("");
+    console.log("entry : " + index);
     console.log("Looking at " + line);
     console.log("found searching left " + leftFind);
     console.log("found searching right " + rightFind);
@@ -23,22 +26,31 @@ const day1 = (data: string) => {
   console.log(sum);
 };
 
+/*
+ this function will find the first 1-9 number or word and it as 1-9 number string.
+
+ Again don't like this strategy Might just make a wrapper function instead of using a flag in the function to know if it needs to worry about reverse search or straight
+*/
 const findFirst = (line: string[], strategy = "straight"): string => {
   let accumulate = "";
 
   for (let current = 0; current < line.length; current++) {
+    // straight search or reverse
     if (strategy === "straight") {
       accumulate += line[current];
     } else {
       accumulate = line[current].concat(accumulate);
     }
 
+    // check if we have a valid single digit
     if (validate(line[current])) {
       accumulate = line[current];
       break;
     }
 
+    // check if we have a valid word
     if (validate(accumulate)) {
+      // return word as digit string.
       accumulate = wordsToNum(accumulate);
       break;
     }
@@ -47,6 +59,7 @@ const findFirst = (line: string[], strategy = "straight"): string => {
   return accumulate;
 };
 
+// validates if given string is a digit 1-9 or string version of it.
 const validate = (input: string): Boolean => {
   let flag = false;
 
@@ -57,11 +70,13 @@ const validate = (input: string): Boolean => {
   return flag;
 };
 
+// take input and finds if it contains the valid string
 const wordsToNum = (input): string => {
   const index = Object.keys(wordsOfNum).find((key) => input.includes(key));
   return `${wordsOfNum[index]}`;
 };
 
+// key value pair object of words of digit.
 const wordsOfNum = {
   zero: 0,
   one: 1,
